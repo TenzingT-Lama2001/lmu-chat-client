bca48007a4586270c3d90243c64f9dc973050a9f
TenzingT-Lama2001 <tenzing.lama36@gmail.com>
2023-07-12 10:58:55 +0545

    Add simple chat

--- app/page.js
+++ app/page.js
@@ -1,113 +1,143 @@
-import Image from 'next/image'
+"use client"
+import { io } from "socket.io-client";
+import React from 'react';
+import axios from "axios";
+import * as chatPb from './chat_pb'
 
 export default function Home() {
+  const [clientId, setClientId] = React.useState('');
+  const [messages, setMessages] = React.useState([]);
+  const [messageText, setMessageText] = React.useState('');
+  const [name, setName] = React.useState('');
+  const [accessToken, setAccessToken] = React.useState('');
+  const [email, setEmail] = React.useState('tenzing@gmail.com');
+  const [password, setPassword] = React.useState('password');
+  const [receiverId, setReceiverId] = React.useState('0e1be047-0112-4591-be79-a41885330966');
+  const [socket, setSocket] = React.useState(null);
+  const [currentUser, setCurrentUser] = React.useState()
+
+React.useEffect(() => {
+  if (accessToken) {
+    const socketInstance = io("http://localhost:3001", {
+      auth: {
+        token: accessToken,
+      },
+    });
+    setSocket(socketInstance);
+
+    socketInstance.on('connect', () => {
+      console.log('SOCKET CONNECTED', socketInstance.id);
+      setClientId(socketInstance.id);
+    });
+
+    socketInstance.on('currentUser', data => {
+      console.log(data)
+      setCurrentUser(data)
+    })
+
+
+    socketInstance.on('previousMessages', (response) => {
+      console.log(`Previous messages: ${response}`);
+      console.log('repsonse', response)
+      
+      setMessages(response);
+
+    });
+
+    socketInstance.on('newMessage', (message) => {
+      console.log('new msg triggered')
+      setMessages(prevMessages => [...prevMessages, message]);
+
+
+      //decoding the array buffer which is received from the backend
+      const decoded = chatPb.Message.deserializeBinary(message.buffer);
+      console.log(decoded.array)
+
+    });
+
+    return () => {
+      socketInstance.disconnect();
+    };
+  }
+}, [accessToken]);
+
+
+
+  const emitSendMessage = async (e) => {
+    e.preventDefault();
+    console.log(`sender id ${currentUser.userId}`)
+    console.log(`receiver id ${receiverId}`)
+
+    const data = {
+      text: messageText,
+      senderId: currentUser.userId,
+      receiverId: receiverId,
+      isGroupChat: false,
+
+    }
+    socket.emit('sendMessage', data)
+    setMessageText('')
+  };
+
+  const login = async (e) => {
+    e.preventDefault();
+    const data = {
+      email,password
+    }
+    try {
+      const response = await axios.post("http://localhost:3001/auth/login", data)
+      console.log(response.data)
+      setAccessToken(response.data.accessToken)
+    }
+    catch (error) {
+        console.log(error)
+    }
+
+
+  }
+  const emitJoinRoom = async (e) => {
+    console.log(receiverId)
+    e.preventDefault();
+    const data = { receiver: receiverId };
+    socket.emit('joinRoom', data, (response) => {
+        console.log(response)
+    })
+   
+  }
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className='flex flex-col min-h-screen w-full items-center justify-center'>
+      <div>
+        <form onSubmit={(e) => login(e)}>
+          <input placeholder="email" value={email } onChange={e => setEmail(e.target.value)} />
+          <input placeholder="password" value={password} onChange={ e => setPassword(e.target.value)} />
+        <button type="submit">Login</button>
+        </form>
+        
+        <input placeholder="Enter room id" value={ receiverId} onChange={e => setReceiverId(e.target.value)} />
+        <button onClick={(e) => emitJoinRoom(e)}>Join</button>
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+        <div className='bg-[#7386FF] w-3/4 h-[70vh] text-white'>
+          <div className='p-3 border-b-2 border-white text-center text-2xl'>Chatty Client</div>
+          <div className={`bg-[#7386FF] border-b-2 border-white h-4/5`}>
+            { messages?.map((message, index) => (
+              <div key={index}>
+                {message.sender.fullName}: {message.message}
+              </div>
+            ) )}
+          </div>
+          <div className=' w-1/2 p-3 bg-white mx-auto mt-1 flex justify-center '>
+            <form onSubmit={emitSendMessage} className="flex justify-evenly w-full">
+              <input className='w-full h-full p-1.5 text-gray-700 outline-none' placeholder='Enter your text...' value={messageText} onChange={(e) => setMessageText(e.target.value)} />
+              <div className='flex'>
+                <button className='bg-[#7386FF] p-1 ml-1' type="submit"  >
+                  Send
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      
     </main>
-  )
+  );
 }
--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  webpack: (config) => {
+    // Alias 'crypto' to 'crypto-browserify'
+    config.resolve.alias.crypto = 'crypto-browserify';
+    return config;
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
